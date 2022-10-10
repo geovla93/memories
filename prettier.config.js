644e4c8d9b785334aebad0e825cc91e6ffccea91
geovla.dev <geovla.dev@gmail.com>
2022-10-10 17:35:30 +0300

    initial setup

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  trailingComma: 'all',
+  semi: true,
+  singleQuote: true,
+  arrowParens: 'always',
+};
